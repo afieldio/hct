b39393ff1bbed62ead6629556dd0b95b011db712
Adam Field <adamf@potatolondon.com>
2018-01-16 21:22:06 +0000

    updates

--- website/models.py
+++ website/models.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Nature(models.Model):
+    species = models.TextField()
+    name = models.TextField()
+    description = models.TextField()
+    image = models.ImageField()
